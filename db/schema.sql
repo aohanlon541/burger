bd3e5a1e73d6125d59a656c979cc14301aa3bae8
Allison O'Hanlon <allison.t.ohanlon@gmail.com>
2017-07-24 22:37:57 -0500

    set up

---
+++ db/schema.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE burger_db;
+
+CREATE TABLE burgers (
+   id INTEGER(11) AUTO_INCREMENT NOT NULL,
+   burger_name VARCHAR(30) NOT NULL,
+   devoured BOOLEAN NOT NULL,
+   date TIMESTAMP,
+   PRIMARY KEY (id)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES (“Big Mac”, False);
+
+INSERT INTO burgers (burger_name, devoured) VALUES (“Chicken Bacon Burger”, False);
+
+INSERT INTO burgers (burger_name, devoured) VALUES (“Bison Burger”, False);
